5a8dc691606f44fe9788872cf8afca61f0766d6a
Apetee <48923553+Apetee@users.noreply.github.com>
2026-03-11 18:36:45 +0100

    Add VariablesExample class with basic variable types

--- java-fundamentals/01-variables/VariablesExample.java
+++ java-fundamentals/01-variables/VariablesExample.java
@@ -1,1 +1,17 @@
+public class VariablesExample {
 
+    public static void main(String[] args) {
+
+        int age = 25;
+        double height = 1.78;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Andres";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
