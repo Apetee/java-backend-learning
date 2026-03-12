f28b94d7472e35d2938f84e3c0b22ef7f70817cc
Apetee <andresgarrido@correo.ugr.es>
2026-03-12 19:34:29 +0100

    added a few CRUD method to studentManager

--- java-fundamentals/arraylist02/StudentManager.java
+++ java-fundamentals/arraylist02/StudentManager.java
@@ -3,17 +3,44 @@ package arraylist02;
 import java.util.ArrayList;
 
 public class StudentManager {
+    private ArrayList<Student> students = new ArrayList<>();
+
+    private void addStudent(Student newStudent){students.add(newStudent);}
+
+    private void removeStudent(String name){
+        for(int i = 0; i < students.size(); i++){
+            if(students.get(i).getName().equals(name)){
+                students.remove(i);
+                break;
+            }
+        }
+    }
+
+    private Student findStudent(String name){
+        for (Student s : students){
+            if (s.getName().equals(name)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    private double averageGrade(){
+        double resultado=0;
+        for(Student s : students){
+            resultado+=s.getGrade();
+        }
+
+        return resultado/students.size();
+    }
+
     public static void main(String[] args){
-        ArrayList<Student> students = new ArrayList<>();
+        StudentManager manager = new StudentManager();
         Student s1 = new Student("Ana", 20, 8);
         Student s2 = new Student("Luis", 22, 7);
         Student s3 = new Student("Maria", 19,9);
-        students.add(s1);
-        students.add(s2);
-        students.add(s3);
 
-        for(Student s : students){
-            System.out.println(s.toString());
-        }
     }
+
+
 }
