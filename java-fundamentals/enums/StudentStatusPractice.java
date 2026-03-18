b459dcd2453a7fdede8b9de831eeda6fdced9c18
Apetee <andresgarrido@correo.ugr.es>
2026-03-18 20:10:02 +0100

    Quick enum refresh

---
+++ java-fundamentals/enums/StudentStatusPractice.java
@@ -0,0 +1,10 @@
+package enums;
+
+public class StudentStatusPractice {
+    public static void main() {
+        StudentStatus ssEnum = StudentStatus.ACTIVE;
+        if(ssEnum==StudentStatus.ACTIVE)
+            System.out.println("Student currently active");
+
+    }
+}
